aaaefff3d3f49b15435e5fb9e9c7db5e23ec1ba1
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-03-28 20:38:57 +0800

    update

--- quantize_awq_mixtral.sh
+++ quantize_awq_mixtral.sh
@@ -3,5 +3,5 @@ export CUDA_VISIBLE_DEVICES=4
 
 for bits in 8 4 2
 do
-    python quantize_mixtral.py --bits $bits --group_size 64
+    python quantize_awq_mixtral.py --bits $bits --group_size 64
 done
