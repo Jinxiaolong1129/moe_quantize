11549f86fed6c1bffed2b8d6563561fc48468009
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-03-28 23:56:57 +0800

    update

--- lm_eval_awq.sh
+++ lm_eval_awq.sh
@@ -5,5 +5,5 @@ for bits in 8 4 2
 do
 python lm_eval_awq.py \
     --model_name mistralai/Mixtral-8x7B-v0.1 \
-    --quant_model_path quantized_Mixtral-8x7B-v0.1-awq-w_bit.$bits-group_size.64
+    --quant_model_path quantized_Mixtral-8x7B-v0.1-awq-w_bit.$bits-group_size.64 >> output_model_bits_$bits.out
 done
