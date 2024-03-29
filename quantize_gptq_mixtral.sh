2bbc6f4f48a370485fb74f096913010fb58fe977
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-03-29 19:35:41 +0800

    update

--- quantize_gptq_mixtral.sh
+++ quantize_gptq_mixtral.sh
@@ -3,7 +3,5 @@ export CUDA_VISIBLE_DEVICES=0,1,2
 
 for bits in 2 4 8
 do
-    python quantize_gptq_mixtral.py \
-        --all_bits $bits \
-        --model_name mistralai/Mixtral-8x7B-v0.1
+    python quantize_gptq_mixtral.py --model_name mistralai/Mixtral-8x7B-v0.1 --all_bits $bits
 done
