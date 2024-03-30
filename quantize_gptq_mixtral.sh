57012a73821d23573db8609db5e8723ea320a45c
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-03-30 09:22:31 +0800

    update

--- quantize_gptq_mixtral.sh
+++ quantize_gptq_mixtral.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=3,4,5
+export CUDA_VISIBLE_DEVICES=0
 export DEBUG=0
 
 python quantize_gptq_mixtral.py --model_name mistralai/Mixtral-8x7B-v0.1 --bits main_8.exp_l1e3_16
