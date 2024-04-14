7c3ff58fb76ac328043b7a41e4feade35ba80fe4
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-04-14 22:31:07 +0800

    update

--- quantize_gptq_mixtral.sh
+++ quantize_gptq_mixtral.sh
@@ -50,3 +50,9 @@ DEBUG=0 CUDA_VISIBLE_DEVICES=0 python quantize_gptq_mixtral.py \
       --model_name mistralai/Mixtral-8x7B-v0.1 \
       --bits main_2.exp_l30e7_8.exp_l31e3_8.exp_l28e4_8.exp_l31e1_8.exp_l29e7_8.exp_l27e2_8.exp_l23e1_8.exp_l8e0_8.exp_l2e0_8.exp_l24e1_8.exp_l12e0_8.exp_l21e0_8.exp_l20e6_8.exp_l12e1_8.exp_l26e4_8.exp_l11e1_8.exp_l31e2_8.exp_l29e6_8.exp_l6e5_8.exp_l17e1_8.exp_l31e7_8.exp_l0e5_8.exp_l0e2_8.exp_l15e2_8.exp_l19e0_8.exp_l4e3_8.exp_l16e3_8.exp_l11e4_8.exp_l18e2_8.exp_l3e2_8.exp_l16e7_8.exp_l10e1_8.exp_l2e1_8.exp_l17e5_8.exp_l1e4_8.exp_l26e3_8.exp_l4e2_8.exp_l9e3_8.exp_l13e4_8.exp_l25e4_8.exp_l14e4_8.exp_l20e2_8.exp_l27e5_8.exp_l24e2_8 \
       --bits_name main_2.task_specific_avg_3bits
+
+# Massive experts in each layer + Task-specific for left (12 experts)
+DEBUG=0 CUDA_VISIBLE_DEVICES=0 python quantize_gptq_mixtral.py \
+      --model_name mistralai/Mixtral-8x7B-v0.1 \
+      --bits main_$bits.exp_l0e0_8.exp_l1e3_8.exp_l2e0_8.exp_l3e4_8.exp_l4e5_8.exp_l5e6_8.exp_l6e4_8.exp_l7e0_8.exp_l8e1_8.exp_l9e7_8.exp_l10e4_8.exp_l11e5_8.exp_l12e2_8.exp_l13e0_8.exp_l14e2_8.exp_l15e3_8.exp_l16e0_8.exp_l17e6_8.exp_l18e0_8.exp_l19e6_8.exp_l20e3_8.exp_l21e6_8.exp_l22e3_8.exp_l23e3_8.exp_l24e2_8.exp_l25e2_8.exp_l26e0_8.exp_l27e4_8.exp_l28e2_8.exp_l29e3_8.exp_l30e0_8.exp_l31e0_8.exp_l30e7_8.exp_l31e3_8.exp_l28e4_8.exp_l31e1_8.exp_l29e7_8.exp_l27e2_8.exp_l23e1_8.exp_l8e0_8.exp_l24e1_8.exp_l12e0_8.exp_l21e0_8.exp_l20e6_8 \
+      --bits_name main_2.task_specific_avg_3bits
