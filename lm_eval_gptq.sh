c421cf4228f24459395bcf8f1510a7603f85b2b8
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-04-21 12:57:15 +0800

    update

--- lm_eval_gptq.sh
+++ lm_eval_gptq.sh
@@ -11,10 +11,10 @@ export PYTHONPATH=/home/LeiFeng/pingzhi/moe_quantize/optimum/:$PYTHONPATH:/home/
 #done
 DEBUG=0 CUDA_VISIBLE_DEVICES=0,1,2 python lm_eval_gptq.py \
     --model_name mistralai/Mixtral-8x7B-v0.1 \
-    --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.most_mass_each_layer_and_wanda_avg_3bits \
+    --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.attn_4.most_mass_avg_3bits \
     --is_quantized
 
-DEBUG=0 CUDA_VISIBLE_DEVICES=5,6,7 python lm_eval_gptq.py \
+DEBUG=0 CUDA_VISIBLE_DEVICES=3,4,5 python lm_eval_gptq.py \
     --model_name mistralai/Mixtral-8x7B-v0.1 \
-    --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.most_mass_each_layer_and_wanda_avg_3bits \
+    --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_3 \
     --is_quantized
