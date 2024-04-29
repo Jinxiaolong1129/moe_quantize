d3c3bdf72dbed1bb4dd84e7169946028da2f7bb8
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-04-29 22:41:21 +0800

    update

--- plot-mixtral-weight.py
+++ plot-mixtral-weight.py
@@ -10,7 +10,7 @@ from transformers import MixtralForCausalLM
 
 plt.rcParams['font.family'] = 'Times New Roman'
 
-
+@torch.no_grad()
 def block_wise_weight_boxplot(save_dir="./results/"):
     model = MixtralForCausalLM.from_pretrained(
         "mistralai/Mixtral-8x7B-v0.1", torch_dtype=torch.bfloat16, device_map="auto"
@@ -20,7 +20,7 @@ def block_wise_weight_boxplot(save_dir="./results/"):
     for block in model.model.layers:
         ffn = block.block_sparse_moe
         ffn_weight = torch.stack(
-            [exp.w1 for exp in ffn.experts] + [exp.w2 for exp in ffn.experts] + [exp.w3 for exp in ffn.experts]
+            [exp.w1.weight.data for exp in ffn.experts] + [exp.w2.weight.data for exp in ffn.experts] + [exp.w3.weight.data for exp in ffn.experts]
         ).flatten().cpu()
         block_flatten_weight.append(ffn_weight)
 
@@ -31,6 +31,23 @@ def block_wise_weight_boxplot(save_dir="./results/"):
     plt.xlabel("Block")
     plt.ylabel("Weight Value")
     plt.save(block_flatten_weight, os.path.join(save_dir, "block_wise_weight_boxplot.png"))
+
+
+@torch.no_grad()
+def expert_wise_weight_boxplot(save_dir="./results/"):
+    model = MixtralForCausalLM.from_pretrained(
+        "mistralai/Mixtral-8x7B-v0.1", torch_dtype=torch.bfloat16, device_map="auto"
+    )
+
+    for block in model.model.layers:
+        ffn = block.block_sparse_moe
+        expert_flatten_weight = torch.stack([
+            torch.cat([exp.w1.weight.data.flatten(), exp.w2.weight.data.flatten(), exp.w3.weight.data.flatten()])
+            for exp in ffn.experts
+        ])
+        # clear plot
+
+
 
 
 if __name__ == "__main__":
