9f0e4a765b67100550252c4758f5fca2f0913118
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-05-01 08:40:58 +0800

    update

--- collect-mixtral-predictor-data.py
+++ collect-mixtral-predictor-data.py
@@ -13,16 +13,15 @@ from transformers.models.mixtral.modeling_mixtral import MixtralForCausalLM, Mix
 
 
 @torch.no_grad()
-def collect_mixtral_predictor_data(
+def collect_mixtral_predictor_train_data(
         seq_len=1024,
         num_samples=400,
-        save_dir="/data/data4/pingzhi/data"
+        save_dir="/data/data4/pingzhi/data/ffn_input_output_pairs"
 ):
     model = MixtralForCausalLM.from_pretrained(
         "mistralai/Mixtral-8x7B-v0.1", torch_dtype=torch.bfloat16, device_map="auto"
     )
     tokenizer = AutoTokenizer.from_pretrained("mistralai/Mixtral-8x7B-v0.1")
-    config = model.config
 
     def _custom_ffn_forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
         input_token = hidden_states.detach().clone().cpu()
@@ -57,6 +56,61 @@ def collect_mixtral_predictor_data(
         with torch.no_grad():
             model(**data)
 
+    save_dir = f"{save_dir}/testset"
+    if not os.path.isdir(save_dir):
+        os.mkdir(save_dir)
+
+    for key, pairs in block_ffn_input_output_pair.items():
+        torch.save(pairs, f"{save_dir}/{key}.pt")
+        print(f"Saved at {save_dir}/{key}.pt")
+
+
+@torch.no_grad()
+def collect_mixtral_predictor_test_data(
+        seq_len=4096,
+        num_samples=128,
+        save_dir="/data/data4/pingzhi/data"
+):
+    model = MixtralForCausalLM.from_pretrained(
+        "mistralai/Mixtral-8x7B-v0.1", torch_dtype=torch.bfloat16, device_map="auto"
+    )
+    tokenizer = AutoTokenizer.from_pretrained("mistralai/Mixtral-8x7B-v0.1")
+    config = model.config
+
+    def _custom_ffn_forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
+        input_token = hidden_states.detach().clone().cpu()
+        original_output = self._original_forward(hidden_states)
+        output_token = original_output[0].detach().clone().cpu()
+        with torch.no_grad():
+            block_ffn_input_output_pair[self._module_name].append((input_token, output_token))
+        return original_output
+
+    block_ffn_input_output_pair = {}
+    for name, module in model.named_modules():
+        if isinstance(module, MixtralSparseMoeBlock):
+            block_ffn_input_output_pair[name] = []
+            module._original_forward = module.forward
+            module._module_name = name
+            module.forward = _custom_ffn_forward.__get__(module, type(module))
+
+    model.eval()
+
+    data = load_dataset("wikitext", "wikitext-2-raw-v1", split="train")
+    # this is GPTQ calibration data
+    text = "".join([" \n" if s == "" else s for s in data["text"][:1000]])
+    encoded_text = tokenizer(text, return_tensors="pt")
+    dataset = []
+    for _ in range(num_samples):
+        i = random.randint(0, encoded_text.input_ids.shape[1] - seq_len - 1)
+        j = i + seq_len
+        inp = encoded_text.input_ids[:, i:j]
+        attention_mask = torch.ones_like(inp)
+        dataset.append({"input_ids": inp, "attention_mask": attention_mask})
+
+    for i, data in enumerate(tqdm(dataset)):
+        with torch.no_grad():
+            model(**data)
+
     save_dir = f"{save_dir}/ffn_input_output_pairs"
     if not os.path.isdir(save_dir):
         os.mkdir(save_dir)
@@ -67,4 +121,4 @@ def collect_mixtral_predictor_data(
 
 
 if __name__ == "__main__":
-    Fire(collect_mixtral_predictor_data)
+    Fire(collect_mixtral_predictor_train_data)
