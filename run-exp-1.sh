9ec3e472e9721bf9980c4880b41f05aedf19b524
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-05-19 23:09:38 +0800

    update

--- run-exp-1.sh
+++ run-exp-1.sh
@@ -1,11 +1,6 @@
-DEBUG=0 CUDA_VISIBLE_DEVICES=3 python quantize_gptq_mixtral.py \
-      --model_name mistralai/Mixtral-8x7B-v0.1 \
-      --bits main_2.attn_2.exp_l4e0_4.exp_l10e0_4.exp_l3e6_4.exp_l7e1_4.exp_l8e5_4.exp_l23e6_4.exp_l24e6_4.exp_l16e5_4.exp_l25e5_4.exp_l26e4_4.exp_l13e7_4.exp_l8e6_4.exp_l2e1_4.exp_l1e3_4.exp_l0e7_4.exp_l11e6_4.exp_l28e6_4.exp_l16e4_4.exp_l21e2_4.exp_l16e5_4.exp_l27e2_4.exp_l0e7_4.exp_l26e6_4 \
-      --bits_name main_2.attn_2.random_23_experts_seed43
-
 DEBUG=0 CUDA_VISIBLE_DEVICES=3,4,5 python lm_eval_gptq.py \
     --model_name mistralai/Mixtral-8x7B-v0.1 \
-    --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.attn_2.random_23_experts_seed43 \
+    --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.attn_4.top8_cos_sim \
     --is_quantized
 
 DEBUG=0 CUDA_VISIBLE_DEVICES=3 python quantize_gptq_mixtral.py \
--- run-exp.sh
+++ run-exp.sh
@@ -1,19 +1,34 @@
-DEBUG=0 CUDA_VISIBLE_DEVICES=0 python quantize_gptq_mixtral.py \
-      --model_name mistralai/Mixtral-8x7B-v0.1 \
-      --bits main_2.attn_2.exp_l20e5_4.exp_l31e4_4.exp_l21e2_4.exp_l31e3_4.exp_l5e7_4.exp_l4e5_4.exp_l10e7_4.exp_l28e4_4 \
-      --bits_name main_2.attn_2.random_8_experts_seed44
-
 DEBUG=0 CUDA_VISIBLE_DEVICES=0,1,2 python lm_eval_gptq.py \
     --model_name mistralai/Mixtral-8x7B-v0.1 \
-    --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.attn_2.random_8_experts_seed44 \
+    --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.attn_4.top4_cos_sim \
     --is_quantized
 
 DEBUG=0 CUDA_VISIBLE_DEVICES=0 python quantize_gptq_mixtral.py \
       --model_name mistralai/Mixtral-8x7B-v0.1 \
-      --bits main_2.attn_2.exp_l6e0_4.exp_l16e5_4.exp_l19e3_4.exp_l22e6_4.exp_l0e2_4.exp_l6e2_4.exp_l24e2_4.exp_l11e6_4.exp_l1e7_4.exp_l18e7_4.exp_l15e7_4.exp_l31e5_4.exp_l21e3_4.exp_l20e6_4.exp_l0e6_4.exp_l21e7_4.exp_l19e7_4.exp_l20e7_4.exp_l0e7_4.exp_l27e1_4.exp_l3e7_4.exp_l24e7_4.exp_l26e5_4 \
-      --bits_name main_2.attn_2.random_23_experts_seed42
+      --bits main_2.attn_2.layer_6_4.layer_13_4.layer_18_4.layer_24_4 \
+      --bits_name main_2.attn_4.random_4_layers_seed42
 
 DEBUG=0 CUDA_VISIBLE_DEVICES=0,1,2 python lm_eval_gptq.py \
     --model_name mistralai/Mixtral-8x7B-v0.1 \
-    --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.attn_2.random_23_experts_seed42 \
+    --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.attn_4.random_4_layers_seed42 \
+    --is_quantized
+
+DEBUG=0 CUDA_VISIBLE_DEVICES=0 python quantize_gptq_mixtral.py \
+      --model_name mistralai/Mixtral-8x7B-v0.1 \
+      --bits main_2.attn_2.layer_4_4.layer_23_4.layer_7_4.layer_5_4 \
+      --bits_name main_2.attn_4.random_4_layers_seed43
+
+DEBUG=0 CUDA_VISIBLE_DEVICES=0,1,2 python lm_eval_gptq.py \
+    --model_name mistralai/Mixtral-8x7B-v0.1 \
+    --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.attn_4.random_4_layers_seed43 \
+    --is_quantized
+
+DEBUG=0 CUDA_VISIBLE_DEVICES=0 python quantize_gptq_mixtral.py \
+      --model_name mistralai/Mixtral-8x7B-v0.1 \
+      --bits main_2.attn_2.layer_20_4.layer_15_4.layer_9_4.layer_19_4 \
+      --bits_name main_2.attn_4.random_4_layers_seed44
+
+DEBUG=0 CUDA_VISIBLE_DEVICES=0,1,2 python lm_eval_gptq.py \
+    --model_name mistralai/Mixtral-8x7B-v0.1 \
+    --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.attn_4.random_4_layers_seed44 \
     --is_quantized
