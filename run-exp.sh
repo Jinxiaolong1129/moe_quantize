484086bb52bc677382bd17a9df7c99dd2ffd4bee
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-05-21 23:06:49 +0800

    update

--- run-exp.sh
+++ run-exp.sh
@@ -1,40 +1,49 @@
-DEBUG=0 CUDA_VISIBLE_DEVICES=0,1,2 python lm_eval_gptq.py \
-    --model_name mistralai/Mixtral-8x7B-v0.1 \
-    --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.attn_4.top4_cos_sim \
-    --is_quantized &
-
-DEBUG=0 CUDA_VISIBLE_DEVICES=6 python quantize_gptq_mixtral.py \
+DEBUG=0 CUDA_VISIBLE_DEVICES=0 python quantize_gptq_mixtral.py \
       --model_name mistralai/Mixtral-8x7B-v0.1 \
-      --bits main_2.attn_4.layer_6_4.layer_13_4.layer_18_4.layer_24_4 \
-      --bits_name main_2.attn_4.random_4_layers_seed42 &
+      --bits main_2.attn_4 \
+      --bits_dict_overwrite results/top_0.25_linear_weight.pt \
+      --bits_name main_2.attn_4.top_0.25_linear_weight &
+
+DEBUG=0 CUDA_VISIBLE_DEVICES=1 python quantize_gptq_mixtral.py \
+      --model_name mistralai/Mixtral-8x7B-v0.1 \
+      --bits main_2.attn_4 \
+      --bits_dict_overwrite results/random_0.25_linear_weight_seed42.pt \
+      --bits_name main_2.attn_4.random_0.25_linear_weight_seed42 &
 
 wait
 
 DEBUG=0 CUDA_VISIBLE_DEVICES=0,1,2 python lm_eval_gptq.py \
     --model_name mistralai/Mixtral-8x7B-v0.1 \
-    --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.attn_4.random_4_layers_seed42 \
+    --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.attn_4.top_0.25_linear_weight \
     --is_quantized &
 
-DEBUG=0 CUDA_VISIBLE_DEVICES=6 python quantize_gptq_mixtral.py \
+DEBUG=0 CUDA_VISIBLE_DEVICES=3 python quantize_gptq_mixtral.py \
       --model_name mistralai/Mixtral-8x7B-v0.1 \
-      --bits main_2.attn_4.layer_4_4.layer_23_4.layer_7_4.layer_5_4 \
-      --bits_name main_2.attn_4.random_4_layers_seed43 &
+      --bits main_2.attn_4 \
+      --bits_dict_overwrite results/random_0.25_linear_weight_seed43.pt \
+      --bits_name main_2.attn_4.random_0.25_linear_weight_seed43 &
 
 wait
 
 DEBUG=0 CUDA_VISIBLE_DEVICES=0,1,2 python lm_eval_gptq.py \
     --model_name mistralai/Mixtral-8x7B-v0.1 \
-    --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.attn_4.random_4_layers_seed43 \
+    --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.attn_4.random_0.25_linear_weight_seed42 \
     --is_quantized &
 
-DEBUG=0 CUDA_VISIBLE_DEVICES=6 python quantize_gptq_mixtral.py \
+DEBUG=0 CUDA_VISIBLE_DEVICES=3 python quantize_gptq_mixtral.py \
       --model_name mistralai/Mixtral-8x7B-v0.1 \
-      --bits main_2.attn_4.layer_20_4.layer_15_4.layer_9_4.layer_19_4 \
-      --bits_name main_2.attn_4.random_4_layers_seed44 &
+      --bits main_2.attn_4 \
+      --bits_dict_overwrite results/random_0.25_linear_weight_seed44.pt \
+      --bits_name main_2.attn_4.random_0.25_linear_weight_seed44 &
 
 wait
 
 DEBUG=0 CUDA_VISIBLE_DEVICES=0,1,2 python lm_eval_gptq.py \
     --model_name mistralai/Mixtral-8x7B-v0.1 \
-    --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.attn_4.random_4_layers_seed44 \
+    --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.attn_4.random_0.25_linear_weight_seed43 \
+    --is_quantized
+
+DEBUG=0 CUDA_VISIBLE_DEVICES=0,1,2 python lm_eval_gptq.py \
+    --model_name mistralai/Mixtral-8x7B-v0.1 \
+    --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.attn_4.random_0.25_linear_weight_seed44 \
     --is_quantized
