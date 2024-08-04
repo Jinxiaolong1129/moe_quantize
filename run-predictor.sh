c964b5ad83887224b6a07961dde325f8a6581c36
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-08-04 19:09:47 +0200

    update

--- run-predictor.sh
+++ run-predictor.sh
@@ -1,4 +1,4 @@
-for id in $(seq 0 31)
+for id in 5 11 17 23
 do
-    CUDA_VISIBLE_DEVICES=0 python run-mixtral-predictor.py --ffn_block_id=$id
+    CUDA_VISIBLE_DEVICES=0 python run-openmoe-predictor.py --ffn_block_id=$id
 done
