abdc161c23d5972e4c50b12c1dde42daebd3c5a7
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-08-05 07:49:40 +0200

    update

--- collect-openmoe-predictor-data.py
+++ collect-openmoe-predictor-data.py
@@ -316,5 +316,99 @@ def collect_openmoe_ffn_with_residual_predictor_test_data(
         print(f"Saved at {save_dir}/{key}.pt")
 
 
+@torch.no_grad()
+def collect_openmoe_ffn_with_residual_cosine_similarity(
+        seq_len=1024,
+        num_samples=400,
+        save_dir="results/ffn_input_output_cosine_similarity"
+):
+    model = OpenMoeForCausalLM.from_pretrained(
+        "OrionZheng/openmoe-8b", torch_dtype=torch.bfloat16, device_map="auto"
+    )
+    tokenizer = OpenMoeTokenizer.from_pretrained("OrionZheng/openmoe-8b")
+
+    def _custom_decoder_forward(
+            self,
+            hidden_states: torch.Tensor,
+            attention_mask: Optional[torch.Tensor] = None,
+            position_ids: Optional[torch.LongTensor] = None,
+            past_key_value: Optional[Tuple[torch.Tensor]] = None,
+            output_attentions: Optional[bool] = False,
+            use_cache: Optional[bool] = False,
+    ) -> Tuple[torch.FloatTensor, Optional[Tuple[torch.FloatTensor, torch.FloatTensor]]]:
+        residual = hidden_states
+
+        hidden_states = self.input_layernorm(hidden_states)
+
+        # Self Attention
+        hidden_states, self_attn_weights, present_key_value = self.self_attn(
+            hidden_states=hidden_states,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            past_key_value=past_key_value,
+            output_attentions=output_attentions,
+            use_cache=use_cache,
+        )
+        hidden_states = residual.to(hidden_states.device) + hidden_states
+
+        # Fully Connected
+        input_token = hidden_states.detach().clone().cpu()  # added
+        residual = hidden_states
+        hidden_states = self.post_attention_layernorm(hidden_states)
+        hidden_states = self.mlp(hidden_states)
+        hidden_states = residual + hidden_states
+
+        if self.moe:
+            residual = hidden_states
+            hidden_states = self.pre_extra_mlp_layernorm(hidden_states)
+            hidden_states = self.extra_mlp(hidden_states)
+            hidden_states = residual + hidden_states
+
+        output_token = hidden_states.detach().clone().cpu()  # added
+        with torch.no_grad():
+            block_ffn_input_output_pair_cos_sim[self._module_name].append(
+                torch.nn.functional.cosine_similarity(input_token, output_token, dim=-1)
+            )  # added
+
+        outputs = (hidden_states,)
+
+        if output_attentions:
+            outputs += (self_attn_weights,)
+
+        if use_cache:
+            outputs += (present_key_value,)
+
+        return outputs
+
+    block_ffn_input_output_pair_cos_sim = {}
+    for name, module in model.named_modules():
+        if isinstance(module, MixtralDecoderLayer):
+            block_ffn_input_output_pair_cos_sim[name] = []
+            module._module_name = name
+            module.forward = _custom_decoder_forward.__get__(module, type(module))
+
+    model.eval()
+
+    data = load_dataset("wikitext", "wikitext-2-raw-v1", split="train")
+    text = "".join([" \n" if s == "" else s for s in data["text"][-2000:]])
+    encoded_text = tokenizer(text, return_tensors="pt")
+    dataset = []
+    for _ in range(num_samples):
+        i = random.randint(0, encoded_text.input_ids.shape[1] - seq_len - 1)
+        j = i + seq_len
+        inp = encoded_text.input_ids[:, i:j]
+        attention_mask = torch.ones_like(inp)
+        dataset.append({"input_ids": inp, "attention_mask": attention_mask})
+
+    for i, data in enumerate(tqdm(dataset)):
+        with torch.no_grad():
+            model(**data)
+
+    if not os.path.isdir(save_dir):
+        os.mkdir(save_dir)
+
+    torch.save(block_ffn_input_output_pair_cos_sim, f"{save_dir}/cosine_similarity.pt")
+
+
 if __name__ == "__main__":
-    Fire(collect_openmoe_ffn_with_residual_predictor_test_data)
+    Fire(collect_openmoe_ffn_with_residual_cosine_similarity)
