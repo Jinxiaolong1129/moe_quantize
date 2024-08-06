5a5611cdf1837e09a0eb3c0e143cf874432cdda4
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-08-07 00:23:13 +0200

    update

--- run-rebuttal-openmoe.sh
+++ run-rebuttal-openmoe.sh
@@ -11,24 +11,24 @@ DEBUG=0 CUDA_VISIBLE_DEVICES=1 python quantize_gptq_openmoe.py \
       --bits main_4.attn_4.layer_5_2.layer_11_2.layer_17_2.layer_23_2 \
       --bits_name main_4.attn_4.layer_5_2.layer_11_2.layer_17_2.layer_23_2
 
-# todo
-
-DEBUG=0 CUDA_VISIBLE_DEVICES=0 python quantize_gptq_openmoe.py \
-      --model_name openmoe-checkpoints/openmoe-8b-native-pt \
-      --bits main_4.attn_4.layer_17_2.layer_23_2 \
-      --bits_name main_4.attn_4.first_2 &
-
-DEBUG=0 CUDA_VISIBLE_DEVICES=1 python quantize_gptq_openmoe.py \
-      --model_name openmoe-checkpoints/openmoe-8b-native-pt \
-      --bits main_4.attn_4.layer_5_2.layer_23_2 \
-      --bits_name main_4.attn_4.predicted_2  &
-
-DEBUG=0 CUDA_VISIBLE_DEVICES=2,3 python lm_eval_gptq_openmoe.py \
-    --model_name openmoe-checkpoints/openmoe-8b-native-pt \
-    --quant_model_path autogptq_openmoe-checkpoints/openmoe-8b-native-pt-gptq_w_bit_main_4.attn_4 \
-    --is_quantized &
-
-DEBUG=0 CUDA_VISIBLE_DEVICES=4,5 python lm_eval_gptq_openmoe.py \
-    --model_name openmoe-checkpoints/openmoe-8b-native-pt \
-    --quant_model_path autogptq_openmoe-checkpoints/openmoe-8b-native-pt-gptq_w_bit_main_4.attn_4.layer_5_2.layer_11_2.layer_17_2.layer_23_2 \
-    --is_quantized &
+## todo
+#
+#DEBUG=0 CUDA_VISIBLE_DEVICES=0 python quantize_gptq_openmoe.py \
+#      --model_name openmoe-checkpoints/openmoe-8b-native-pt \
+#      --bits main_4.attn_4.layer_17_2.layer_23_2 \
+#      --bits_name main_4.attn_4.first_2 &
+#
+#DEBUG=0 CUDA_VISIBLE_DEVICES=1 python quantize_gptq_openmoe.py \
+#      --model_name openmoe-checkpoints/openmoe-8b-native-pt \
+#      --bits main_4.attn_4.layer_5_2.layer_23_2 \
+#      --bits_name main_4.attn_4.predicted_2  &
+#
+#DEBUG=0 CUDA_VISIBLE_DEVICES=2,3 python lm_eval_gptq_openmoe.py \
+#    --model_name openmoe-checkpoints/openmoe-8b-native-pt \
+#    --quant_model_path autogptq_openmoe-checkpoints/openmoe-8b-native-pt-gptq_w_bit_main_4.attn_4 \
+#    --is_quantized &
+#
+#DEBUG=0 CUDA_VISIBLE_DEVICES=4,5 python lm_eval_gptq_openmoe.py \
+#    --model_name openmoe-checkpoints/openmoe-8b-native-pt \
+#    --quant_model_path autogptq_openmoe-checkpoints/openmoe-8b-native-pt-gptq_w_bit_main_4.attn_4.layer_5_2.layer_11_2.layer_17_2.layer_23_2 \
+#    --is_quantized &
