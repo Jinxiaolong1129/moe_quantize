375294bad9716abe199521240e826df377799860
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-08-07 01:17:59 +0200

    update

--- run-rebuttal-openmoe.sh
+++ run-rebuttal-openmoe.sh
@@ -1,23 +1,23 @@
 #export PYTHONPATH=/data2/pzli/moe_quantize/optimum/onnxruntime/:/data2/pzli/moe_quantize/optimum/:/data2/pzli/moe_quantize/auto_gptq/:$PYTHONPATH
-
-DEBUG=0 CUDA_VISIBLE_DEVICES=0 python quantize_gptq_openmoe.py \
-      --model_name openmoe-checkpoints/openmoe-8b-native-pt \
-      --bits main_4.attn_4 \
-      --bits_name main_4.attn_4 &
-
-# 2.41 bits
-DEBUG=0 CUDA_VISIBLE_DEVICES=1 python quantize_gptq_openmoe.py \
-      --model_name openmoe-checkpoints/openmoe-8b-native-pt \
-      --bits main_4.attn_4.layer_5_2.layer_11_2.layer_17_2.layer_23_2 \
-      --bits_name main_4.attn_4.layer_5_2.layer_11_2.layer_17_2.layer_23_2
-
-## todo
 #
 #DEBUG=0 CUDA_VISIBLE_DEVICES=0 python quantize_gptq_openmoe.py \
 #      --model_name openmoe-checkpoints/openmoe-8b-native-pt \
-#      --bits main_4.attn_4.layer_17_2.layer_23_2 \
-#      --bits_name main_4.attn_4.first_2 &
+#      --bits main_4.attn_4 \
+#      --bits_name main_4.attn_4 &
 #
+## 2.41 bits
+#DEBUG=0 CUDA_VISIBLE_DEVICES=1 python quantize_gptq_openmoe.py \
+#      --model_name openmoe-checkpoints/openmoe-8b-native-pt \
+#      --bits main_4.attn_4.layer_5_2.layer_11_2.layer_17_2.layer_23_2 \
+#      --bits_name main_4.attn_4.layer_5_2.layer_11_2.layer_17_2.layer_23_2
+
+## todo
+#
+DEBUG=0 CUDA_VISIBLE_DEVICES=0 python quantize_gptq_openmoe.py \
+      --model_name openmoe-checkpoints/openmoe-8b-native-pt \
+      --bits main_4.attn_4.layer_17_2.layer_23_2 \
+      --bits_name main_4.attn_4.first_2 &
+
 #DEBUG=0 CUDA_VISIBLE_DEVICES=1 python quantize_gptq_openmoe.py \
 #      --model_name openmoe-checkpoints/openmoe-8b-native-pt \
 #      --bits main_4.attn_4.layer_5_2.layer_23_2 \
