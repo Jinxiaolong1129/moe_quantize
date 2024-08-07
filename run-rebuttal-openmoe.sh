f5b8f883a7428fe6c4cad1c99dce4f987ff7b71c
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-08-07 05:57:07 +0200

    update

--- run-rebuttal-openmoe.sh
+++ run-rebuttal-openmoe.sh
@@ -18,12 +18,12 @@
 #      --bits main_4.attn_4.layer_17_2.layer_23_2 \
 #      --bits_name main_4.attn_4.first_2 &
 
-DEBUG=0 CUDA_VISIBLE_DEVICES=2 python quantize_gptq_openmoe.py \
+DEBUG=0 CUDA_VISIBLE_DEVICES=6 python quantize_gptq_openmoe.py \
       --model_name openmoe-checkpoints/openmoe-8b-native-pt \
       --bits main_4.attn_4.layer_5_2.layer_11_2.layer_23_2 \
       --bits_name main_4.attn_4.predicted_1 &
 
-DEBUG=0 CUDA_VISIBLE_DEVICES=1 python quantize_gptq_openmoe.py \
+DEBUG=0 CUDA_VISIBLE_DEVICES=7 python quantize_gptq_openmoe.py \
       --model_name openmoe-checkpoints/openmoe-8b-native-pt \
       --bits main_4.attn_4.layer_11_2.layer_17_2.layer_23_2 \
       --bits_name main_4.attn_4.first_1  &
