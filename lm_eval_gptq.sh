03fea7c140165de190c77b4d729a0af8dcdf66c7
Pingzhi Li <pingzhi@cs.unc.edu>
2024-11-22 15:24:32 -0500

    sync

--- lm_eval_gptq.sh
+++ lm_eval_gptq.sh
@@ -11,10 +11,10 @@ export PYTHONPATH=/home/LeiFeng/pingzhi/moe_quantize/optimum/:$PYTHONPATH:/home/
 #done
 DEBUG=0 CUDA_VISIBLE_DEVICES=0,1,2 python lm_eval_gptq.py \
     --model_name mistralai/Mixtral-8x7B-v0.1 \
-    --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.attn_4.random4_per_layer_seed43 \
-    --is_quantized
+    --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.attn_4.random2_per_layer_seed42 \
+    --is_quantized &
 
 DEBUG=0 CUDA_VISIBLE_DEVICES=3,4,5 python lm_eval_gptq.py \
     --model_name mistralai/Mixtral-8x7B-v0.1 \
     --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.attn_4.random4_per_layer_seed44 \
-    --is_quantized
+    --is_quantized &
