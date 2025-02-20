75be55ab93df27986ac8d811728c426931f55295
pingzhi <pingzhili@mail.ustc.edu.cn>
2025-02-20 03:08:17 -0500

    update

--- lm_eval_gptq.py
+++ lm_eval_gptq.py
@@ -17,9 +17,9 @@ from lm_eval.models.huggingface import HFLM
 from lm_eval.tasks import initialize_tasks
 
 LM_EVAL_TASK_KWARGS_DICT = {
+    "openbookqa": {"task": "openbookqa", "num_fewshot": 0, "batch_size": 128, "metric": "acc_norm"},
     "winogrande": {"task": "winogrande", "num_fewshot": 0, "batch_size": 128, "metric": "acc"},
     "copa": {"task": "copa", "num_fewshot": 0, "batch_size": 128, "metric": "acc"},
-    "openbookqa": {"task": "openbookqa", "num_fewshot": 0, "batch_size": 128, "metric": "acc_norm"},
     "hellaswag": {"task": "hellaswag", "num_fewshot": 0, "batch_size": 128, "metric": "acc_norm"},
     # "lambada_openai": {"task": "lambada_openai", "num_fewshot": 0, "batch_size": 128, "metric": "acc"},
     # "rte": {"task": "rte", "num_fewshot": 0, "batch_size": 128, "metric": "acc"},
