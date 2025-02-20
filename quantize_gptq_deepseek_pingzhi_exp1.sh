210b3e5221ceee3c0b48df50e60d0956e149b454
Jinxiaolong1129 <inxiaolong1129@gmail.com>
2025-02-20 02:31:57 -0500

    update bash

--- quantize_gptq_deepseek_pingzhi_exp1.sh
+++ quantize_gptq_deepseek_pingzhi_exp1.sh
@@ -2,7 +2,7 @@
 export PYTHONPATH=auto_gptq/:$PYTHONPATH
 
 
-export CUDA_VISIBLE_DEVICES=7
+export CUDA_VISIBLE_DEVICES=4
 nohup python quantize_gptq_deepseek_layer.py \
     --model_name deepseek-ai/deepseek-moe-16b-base \
     --nsamples 512 \
@@ -11,7 +11,7 @@ nohup python quantize_gptq_deepseek_layer.py \
     > run_log/pingzhi_exp1/moe.shared_4.other_2+other_block.4.out &
 
 
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=5
 nohup python quantize_gptq_deepseek_layer.py \
     --model_name deepseek-ai/deepseek-moe-16b-base \
     --nsamples 512 \
@@ -20,7 +20,7 @@ nohup python quantize_gptq_deepseek_layer.py \
     > run_log/pingzhi_exp1/moe.shared_8.other_2+other_block.4.out &
 
 
-export CUDA_VISIBLE_DEVICES=4
+export CUDA_VISIBLE_DEVICES=6
 nohup python quantize_gptq_deepseek_layer.py \
     --model_name deepseek-ai/deepseek-moe-16b-base \
     --nsamples 512 \
@@ -29,7 +29,7 @@ nohup python quantize_gptq_deepseek_layer.py \
     > run_log/pingzhi_exp1/moe.shared_3.other_2+other_block.4.out &
 
 
-export CUDA_VISIBLE_DEVICES=5
+export CUDA_VISIBLE_DEVICES=7
 nohup python quantize_gptq_deepseek_layer.py \
     --model_name deepseek-ai/deepseek-moe-16b-base \
     --nsamples 512 \
