ef33261e1a1c7df5065921d61a62c135e09568e9
Pingzhi Li <pingzhi@cs.unc.edu>
2025-02-23 00:07:16 -0500

    sync

--- run-exp-kdd.sh
+++ run-exp-kdd.sh
@@ -1,9 +1,9 @@
-DEBUG=0 CUDA_VISIBLE_DEVICES=0,1,3 CUDA_LAUNCH_BLOCKING=1 python lm_eval_gptq.py \
-    --model_name mistralai/Mixtral-8x7B-v0.1 \
-    --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.attn_4.frequency_top5_per_layer \
-    --is_quantized
-
-DEBUG=0 CUDA_VISIBLE_DEVICES=0,1,3 CUDA_LAUNCH_BLOCKING=1 python lm_eval_gptq.py \
+DEBUG=0 CUDA_VISIBLE_DEVICES=0,2,3 CUDA_LAUNCH_BLOCKING=1 python lm_eval_gptq.py \
     --model_name mistralai/Mixtral-8x7B-v0.1 \
     --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.attn_4.frequency_top6_per_layer \
+    --is_quantized
+
+DEBUG=0 CUDA_VISIBLE_DEVICES=0,2,3 CUDA_LAUNCH_BLOCKING=1 python lm_eval_gptq.py \
+    --model_name mistralai/Mixtral-8x7B-v0.1 \
+    --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.attn_4.frequency_top5_per_layer \
     --is_quantized
