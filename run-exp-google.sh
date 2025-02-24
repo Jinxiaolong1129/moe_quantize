b3b214f03371cd86291c5809207d2981bbdd2850
Pingzhi Li <pingzhi@cs.unc.edu>
2025-02-24 13:41:08 -0500

    sync

--- run-exp-google.sh
+++ run-exp-google.sh
@@ -1,11 +1,11 @@
 export HF_HOME=./cache/huggingface
 
-DEBUG=0 CUDA_VISIBLE_DEVICES=0 python quantize_gptq_mixtral.py \
+DEBUG=0 CUDA_VISIBLE_DEVICES=0,1 python quantize_gptq_mixtral.py \
       --model_name mistralai/Mixtral-8x7B-v0.1 \
       --bits main_2.attn_4.layer_0_4.layer_1_4.layer_2_4.layer_3_4.keyword__w2__4 \
       --bits_name main_2.attn_4.first_4_layer_and_all_w2 &
 
-DEBUG=0 CUDA_VISIBLE_DEVICES=1 python quantize_gptq_mixtral.py \
+DEBUG=0 CUDA_VISIBLE_DEVICES=2,3 python quantize_gptq_mixtral.py \
       --model_name mistralai/Mixtral-8x7B-v0.1 \
       --bits main_2.attn_4.layer_0_4.layer_1_4.layer_2_4.layer_3_4.layer_4_4.layer_5_4.keyword__w2__4 \
       --bits_name main_2.attn_4.first_6_layer_and_all_w2 &
