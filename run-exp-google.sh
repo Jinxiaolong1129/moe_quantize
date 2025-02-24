7a74c9fcac1eee45019d2d9b4d6795fd241f4b82
Pingzhi Li <pingzhi@cs.unc.edu>
2025-02-24 17:41:40 -0500

    sync

--- run-exp-google.sh
+++ run-exp-google.sh
@@ -12,12 +12,12 @@ DEBUG=0 CUDA_VISIBLE_DEVICES=1 python quantize_gptq_mixtral.py \
 
 wait
 
-DEBUG=0 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 CUDA_LAUNCH_BLOCKING=1 python lm_eval_gptq.py \
+DEBUG=0 CUDA_VISIBLE_DEVICES=0,1,2 CUDA_LAUNCH_BLOCKING=1 python lm_eval_gptq.py \
     --model_name mistralai/Mixtral-8x7B-v0.1 \
     --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.attn_4.first_12_layer_and_all_w2 \
     --is_quantized
 
-DEBUG=0 CUDA_VISIBLE_DEVICES=8,9,10,11,12,13,14,15 CUDA_LAUNCH_BLOCKING=1 python lm_eval_gptq.py \
+DEBUG=0 CUDA_VISIBLE_DEVICES=0,1,2 CUDA_LAUNCH_BLOCKING=1 python lm_eval_gptq.py \
     --model_name mistralai/Mixtral-8x7B-v0.1 \
     --quant_model_path autogptq_mistralai/Mixtral-8x7B-v0.1-gptq_w_bit_main_2.attn_4.first_16_layer_and_all_w2 \
     --is_quantized
